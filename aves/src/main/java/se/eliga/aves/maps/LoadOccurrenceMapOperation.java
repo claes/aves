005349c2861dab7a26fbd93a0b332e1f5adfa0ad
Claes Holmerson <claes@holmerson.net>
2015-05-15 22:05:39 +0200

    Added license and copyright notice

--- aves/src/main/java/se/eliga/aves/maps/LoadOccurrenceMapOperation.java
+++ aves/src/main/java/se/eliga/aves/maps/LoadOccurrenceMapOperation.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) Claes Holmerson 2013, 2015. The program is licensed under GNU GPL v3. See LICENSE.txt for details.
+ */
+
 package se.eliga.aves.maps;
 
 import android.os.AsyncTask;
